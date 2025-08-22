53b1ce73e4ff5a41970e07c06de8fa3adc8ada30
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-08-22 10:12:03 +0100

    feat: basic state machine logic

--- lib/atm_statem.ex
+++ lib/atm_statem.ex
@@ -1,18 +1,92 @@
 defmodule AtmStatem do
-  @moduledoc """
-  Documentation for `AtmStatem`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  @behaviour :gen_statem
 
-  ## Examples
+  @name :atm_statem
 
-      iex> AtmStatem.hello()
-      :world
+  @idle_message "Please insert card"
+  @pin_message "Please enter PIN"
+  @cash_message "Insert cash amount"
 
-  """
-  def hello do
-    :world
+  def callback_mode, do: :state_functions
+
+  # APIs
+  def start do
+    :gen_statem.start({:local, @name}, __MODULE__, [], [])
+  end
+
+  def stop do
+    :gen_statem.stop(@name)
+  end
+
+  def message do
+    :gen_statem.call(@name, :message)
+  end
+
+  def insert_card do
+    :gen_statem.call(@name, :insert_card)
+  end
+
+  def insert_pin(pin) when is_binary(pin) do
+    case correct_pin?(pin) do
+      true -> :gen_statem.call(@name, :insert_correct_pin)
+      false -> :gen_statem.call(@name, :insert_wrong_pin)
+    end
+  end
+
+  def insert_amount(_amount) do
+    :gen_statem.call(@name, :insert_amount)
+  end
+
+  # Mandatory callbacks
+  def init([]) do
+    {:ok, :idle, @idle_message}
+  end
+
+  def terminate(reason, _state, _data) do
+    Logger.info("#{@name} terminated with reason - #{reason}")
+  end
+
+  # State callbacks
+  def idle({:call, from}, :insert_card, _data) do
+    {:next_state, :pin, @pin_message, [{:reply, from, :pin}]}
+  end
+
+  def idle(event_type, event_content, data) do
+    handle_event(event_type, event_content, data)
+  end
+
+  def pin({:call, from}, :insert_correct_pin, _data) do
+    {:next_state, :cash, @cash_message, [{:reply, from, :cash}]}
+  end
+
+  def pin({:call, from}, :insert_wrong_pin, _data) do
+    {:next_state, :idle, @idle_message, [{:reply, from, :idle}]}
+  end
+
+  def pin(event_type, event_content, data) do
+    handle_event(event_type, event_content, data)
+  end
+
+  def cash({:call, from}, :insert_amount, _data) do
+    {:next_state, :idle, @idle_message, [{:reply, from, :idle}]}
+  end
+
+  def cash(event_type, event_content, data) do
+    handle_event(event_type, event_content, data)
+  end
+
+  # Internal functions
+  defp handle_event({:call, from}, :message, data) do
+    {:keep_state, data, [{:reply, from, data}]}
+  end
+
+  defp handle_event({:call, from}, _, data) do
+    {:keep_state, data, [{:reply, from, data}]}
+  end
+
+  defp correct_pin?(pin) do
+    !String.starts_with?(pin, "0")
   end
 end
--- test/atm_statem_test.exs
+++ test/atm_statem_test.exs
@@ -2,7 +2,33 @@ defmodule AtmStatemTest do
   use ExUnit.Case
   doctest AtmStatem
 
-  test "greets the world" do
-    assert AtmStatem.hello() == :world
+  import ExUnit.CaptureLog
+
+  setup do
+    {:ok, pid} = AtmStatem.start()
+
+    on_exit(fn ->
+      assert capture_log(fn -> AtmStatem.stop() end) =~
+               "[info] atm_statem terminated with reason - normal"
+
+      refute Process.alive?(pid)
+    end)
+  end
+
+  test "state machine with valid actions" do
+    assert AtmStatem.message() == "Please insert card"
+    assert AtmStatem.insert_card() == :pin
+    assert AtmStatem.message() == "Please enter PIN"
+    assert AtmStatem.insert_pin("111") == :cash
+    assert AtmStatem.message() == "Insert cash amount"
+    assert AtmStatem.insert_amount(100) == :idle
+  end
+
+  test "state machine with invalid actions" do
+    assert AtmStatem.message() == "Please insert card"
+    assert AtmStatem.insert_amount(100) == "Please insert card"
+    assert AtmStatem.insert_card() == :pin
+    assert AtmStatem.insert_amount(100) == "Please enter PIN"
+    assert AtmStatem.insert_pin("0111") == :idle
   end
 end
